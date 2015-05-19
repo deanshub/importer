bdca649e3225329c393f2dcc1b2588fef8f53c26
Dean Shub <dean.shub@sisense.com>
2015-05-19 15:15:53 +0300

    making import export promised

--- app/controller.js
+++ app/controller.js
@@ -2,8 +2,10 @@
 var browser = require('./browser');
 var remote = browser.remote;
 var mongo = remote.require('mongoskin');
+var Q = remote.require('q');
+var $ = require('jquery');
 
-function mainCtrl(dataSources) {
+function mainCtrl(dataSources, $scope) {
 	var self = this;
 
 	function init(){
@@ -27,85 +29,108 @@ function mainCtrl(dataSources) {
 		return target+'/fromForm.html';
 	};
 
+	function getDbUrl(connObj){
+		var url = 'mongodb://';
+		if (connObj.user) {
+			url+=connObj.user;
+			if (connObj.password) {
+				url+=':'+connObj.password;
+
+			}	
+			url+='@';
+		}
+
+		url+=connObj.host||'localhost';
+		if (connObj.port){
+			url+=':'+connObj.port;
+		}
+
+		if(connObj.db){
+			url+='/'+connObj.db;
+			return url;
+		}else{
+			return null;
+		}
+	}
+
 	self.importer = function(){
-		var imported;
 		self.loading = true;
 
-		// source handling
-		if (self.selectedSource==='File'){
-			if (/\.json$/i.test(self.fromForm.file.path)){
-				try{
-					imported = remote.require(self.fromForm.file.path);
-				}catch(e){
-					console.log(e);
-					self.error = self.fromForm.file.path + ' is not a valid json file';
-				}
-			}else if (/\.csv$/i.test(self.fromForm.file.path)){
-				// parse csv
-			}else{
-				// try both
-			}
-			console.log(JSON.stringify(imported));
-		}else if (self.selectedSource==='URL'){
-		}else if (self.selectedSource==='Text'){
-			// try json parsing
-			try{
-				imported = JSON.parse(self.fromForm.text);
-			}catch(e){
-				// try csv parsing
-
-			}
-		}else if (self.selectedSource==='MongoDB'){
-
-		}
-
-
-
-		if (!imported && !self.error){
-			// if no errors present one
-			self.error = 'This source can\'t be imported';
-		// target handling
-		}else if (imported) {
-			if (self.selectedTarget==='File') {
-				self.loading = false;
-			}else if (self.selectedTarget==='Text'){
-				self.toForm.text = JSON.stringify(imported,null,2);
-				self.loading = false;
-			}else if (self.selectedTarget==='MongoDB'){
-				var url = 'mongodb://';
-				if (self.toForm.user) {
-					url+=self.toForm.user;
-					if (self.toForm.password) {
-						url+=':'+self.toForm.password;
-
-					}	
-					url+='@';
-				}
-
-				url+=self.toForm.host||'localhost';
-				if (self.toForm.port){
-					url+=':'+self.toForm.port;
-				}
-
-				if(self.toForm.db){
-					url+='/'+self.toForm.db;
-				}else{
-					self.error = 'DB name not provided';
-					self.loading = false;
-					return;
-				}
-
-				var db = mongo.db(url);
-				db.collection(self.toForm.collection).insert(imported,function(err){
-					if (err){
-						self.loading = false;
-						self.error = 'Could not insert to the specified collection';
-						console.log(err);
+		Q.promise(function(resolve,reject){
+			// source handling
+			if (self.selectedSource==='File'){
+				if (/\.json$/i.test(self.fromForm.file.path)){
+					try{
+						resolve(remote.require(self.fromForm.file.path));
+					}catch(e){
+						reject(self.fromForm.file.path + ' is not a valid json file');
 					}
-					db.close();
-				});
+				}else if (/\.csv$/i.test(self.fromForm.file.path)){
+					// parse csv
+				}else{
+					// try both
+				}
+			}else if (self.selectedSource==='URL'){
+			}else if (self.selectedSource==='Text'){
+				// try json parsing
+				try{
+					resolve(JSON.parse(self.fromForm.text));
+				}catch(e){
+					// try csv parsing
+
+				}
+			}else if (self.selectedSource==='MongoDB'){
+				var url = getDbUrl(self.fromForm);
+				if (url){
+					var db = mongo.db(url);
+					db.collection(self.fromForm.collection).find().toArray(function(err,result){
+						if(err){
+							reject('could not get specified collection form data source');
+						}
+						resolve(result);
+					});
+				}else{
+					reject('DB name not provided in data source');
+				}
 			}
-		}
+		}).then(function(imported){
+			return Q.promise(function(resolve, reject){
+				if (!imported){
+					// if no errors present one
+					reject('This source can\'t be imported');
+				// target handling
+				}else if (imported) {
+					if (self.selectedTarget==='File') {
+						resolve();
+					}else if (self.selectedTarget==='Text'){						
+						$scope.$apply(function(){
+							$('#toFormText').html(JSON.stringify(imported,null,2).replace(/\n/g,'<br/>'));
+						});
+						resolve();
+					}else if (self.selectedTarget==='MongoDB'){
+						var url = getDbUrl(self.toForm);
+						if (url){
+							var db = mongo.db(url);
+							db.collection(self.toForm.collection).insert(imported,function(err){
+								if (err){
+									reject('Could not insert to the specified collection');
+								}
+								db.close();
+								resolve();
+							});
+						}else{
+							reject('DB name not provided in data target');
+						}
+					}
+				}
+			});
+		}).then(function(){
+			self.loading = false;
+		}).catch(function(err){
+			console.log(err);
+			self.loading=false;
+			self.error = err;
+		})
 	};
 
 	init();
@@ -113,5 +138,6 @@ function mainCtrl(dataSources) {
 
 module.exports = [
 'dataSources',
+'$scope',
 mainCtrl
 ];
